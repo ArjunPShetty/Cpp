eb4837bb0b53418a41a1be5fb91f166f24478602
Arjun  P Shetty <arjunpshetty.0@gmail.com>
2026-01-14 11:02:40 +0530

    added the  code file

---
+++ operaters.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a=5, b=4;
+    
+    cout<<"\n The Operaters are = "<<a+b;
+    cout<<"\n The Operaters are = "<<a-b;
+    cout<<"\n The Operaters are = "<<a*b;
+    cout<<"\n The Operaters are = "<<a/b;
+    cout<<"\n The Operaters are = "<<a%b;
+    cout<<"\n The Operaters are = "<<a++;
+    cout<<"\n The Operaters are = "<<a--;
+    cout<<"\n The Operaters are = "<<++a;
+    cout<<"\n The Operaters are = "<<--a;
+
+
+    
+    return 0;
+}
