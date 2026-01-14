5f04ab8586793d80fe21b6e7c944b06e24b7fdd9
Arjun  P Shetty <arjunpshetty.0@gmail.com>
2026-01-14 22:39:44 +0530

    added the Comperation operation file

---
+++ Comperation operation.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a=5,b=4;
+    cout<<"The Comperation Equal operation = "<<(a==b)<<endl;
+    cout<<"The Comperation Greater than or equal to operation = "<<(a>=b)<<endl;
+    cout<<"The Comperation Less than or equal to operation = "<<(a<=b)<<endl;
+    cout<<"The Comperation Greater than operation = "<<(a>b)<<endl;
+    cout<<"The Comperation Less than operation = "<<(a<b)<<endl;
+    cout<<"The Comperation Not equal to operation = "<<(a!=b)<<endl;
+    return 0;
+}
