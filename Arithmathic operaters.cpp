b7b2292e35e8dec2415b69b86fddcb3aa329a63f
Arjun  P Shetty <arjunpshetty.0@gmail.com>
2026-01-14 20:35:25 +0530

    added the Arithmathic operaters file

---
+++ Arithmathic operaters.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a=5, b=4;
+    
+    cout<<"\n The Arithmathic Addition Operaters are = "<<a+b;
+    cout<<"\n The Arithmathic Subtraction Operaters are = "<<a-b;
+    cout<<"\n The Arithmathic Multiplication Operaters are = "<<a*b;
+    cout<<"\n The Arithmathic Division Operaters are = "<<a/b;
+    cout<<"\n The Arithmathic Modulo Operaters are = "<<a%b;
+    cout<<"\n The Arithmathic Increment Operaters are = "<<a++;
+    cout<<"\n The Arithmathic Decrement Operaters are = "<<a--;
+    cout<<"\n The Arithmathic Pre-Increment Operaters are = "<<++a;
+    cout<<"\n The Arithmathic Pre-Decrement Operaters are = "<<--a;    
+    return 0;
+}
