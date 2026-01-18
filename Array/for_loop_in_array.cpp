dea0a572151ad3c560e77d57b6cd8299cc6ee012
Arjun  P Shetty <arjunpshetty.0@gmail.com>
2026-01-18 15:25:20 +0530

    added the for_loop_in_array.cpp file

---
+++ Array/for_loop_in_array.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string arr[] = {"india","Russia","USA","China","UK","France","Germany","Italy","Canada"};
+    for(int i=0;i<=8;i++){
+    cout<<arr[i]<<endl; 
+    }
+    return 0;
+}
