3ba5ed51e30e5fa771ea6e1e96ad3e9a7304b9b6
Arjun  P Shetty <arjunpshetty.0@gmail.com>
2026-01-18 13:42:52 +0530

    added the subtraction_in_pointer.cpp file

---
+++ pointer/subtraction_in_pointer.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    
+    int a=10;
+    int*ptr = &a;
+    cout<<ptr<<endl;
+    ptr=ptr-1;   //-4 bits subtracted (1 int = 4 bit)
+    cout<<ptr<<endl;
+    
+    return 0;
+}
