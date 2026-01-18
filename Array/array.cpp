8c473d9c8f0d966e885e74e1b5fb6c39c081806d
Arjun  P Shetty <arjunpshetty.0@gmail.com>
2026-01-18 15:24:40 +0530

    added the array.cpp file

---
+++ Array/array.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string arr[] = {"india","Russia","USA","China","UK","France","Germany","Italy","Canada"};
+    cout<<arr[0]<<endl; 
+    cout<<arr[1]<<endl; 
+    cout<<arr[2]<<endl; 
+    cout<<arr[3]<<endl; 
+    cout<<arr[4]<<endl; 
+    cout<<arr[5]<<endl; 
+    cout<<arr[6]<<endl; 
+    cout<<arr[7]<<endl; 
+    cout<<arr[8]<<endl; 
+    return 0;
+}
