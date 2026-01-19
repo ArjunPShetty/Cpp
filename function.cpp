eb3c494878ef6ee7134a45e7bb3cb577fe96f92b
Arjun  P Shetty <arjunpshetty.0@gmail.com>
2026-01-19 15:10:13 +0530

    added the function.cppfunction.cpp file

---
+++ function.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int sum(int a, int b) {     //function with return type
+        int c = a + b;
+    return c;
+}
+
+int main() {
+    int num1, num2;
+
+    cout << "Enter the first number = " << endl;
+    cin >> num1;
+
+    cout << "Enter the second number = " << endl;
+    cin >> num2;
+
+    cout << "The sum is = " << sum(num1, num2);
+
+    return 0;              //return value function key
+    }
