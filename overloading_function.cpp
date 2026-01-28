88018475becc408a8ba9b846fc4c25e80ece3fe4
Arjun  P Shetty <arjunpshetty.0@gmail.com>
2026-01-28 15:56:28 +0530

    added the overloading_function.cpp file

---
+++ overloading_function.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int sum(int a,int b, int c) {
+    return a+b+c;
+}
+int sum(int a,int b) {
+    return a+b;
+}
+int main() {
+
+    cout << "The sum of 3 and 4 is "<<sum(3,4)<< endl;
+    cout << "The sum of 3, 4and 8 is "<<sum(3,4,8)<< endl;
+    return 0;
+}
